53f5d15f14189a7bc04701e4541a7e0bfc7486af
github username <github email address>
2018-02-13 15:09:19 -0500

    sign up authorization complete

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -24,4 +24,28 @@ class Api::V1::AuthController < ApplicationController
        }, status: 404
      end
    end
+
+
+   def signUp
+     @user = User.new(user_params)
+     if @user.save
+       created_jwt = issue_token({id: @user.id})
+       render json: {username: @user.username,jwt: created_jwt}
+     else
+       render json: {errors: @user.errors.full_messages}, status: 422
+     end
+   end
+
+
+   private
+
+   def user_params
+     params.require(:user).permit(
+       :username,
+       :first_name,
+       :last_name,
+       :password,
+       :password_confirmation
+     )
+   end
 end
